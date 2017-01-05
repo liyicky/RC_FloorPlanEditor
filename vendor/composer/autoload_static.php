cfa9bbb3a5b4358c0b7f5bf7e3bb33d1a7ef6bb9
liyicky <liyicky@gmail.com>
2017-01-05 18:41:00 +0900

    Add Composer Twig

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,9 +6,20 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit794585604eb66af6761534c246c9baba
 {
+    public static $prefixesPsr0 = array (
+        'T' => 
+        array (
+            'Twig_' => 
+            array (
+                0 => __DIR__ . '/..' . '/twig/twig/lib',
+            ),
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixesPsr0 = ComposerStaticInit794585604eb66af6761534c246c9baba::$prefixesPsr0;
 
         }, null, ClassLoader::class);
     }
